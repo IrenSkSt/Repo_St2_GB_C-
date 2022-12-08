06ccd22b7177dac94ea35574ad1cf6381b0ab41d
IrenSkSt <skiv.study@gmail.com>
2022-12-09 02:54:59 +0300

    Correct desides hw_2

--- hw_2_Sk/Program.cs
+++ hw_2_Sk/Program.cs
@@ -1,35 +1,29 @@
 ﻿// Задача 1 - Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 /*
-Console.Write("Ведите трехзначное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int result = -1;
-
 int NumDes(int num)
 {
-    return result = number / 10 % 10;
+    return num / 10 % 10;
 }
+
+Console.Write("Ведите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
 
 if (number > 100 && number < 1000)
 {
-    result = NumDes(number);
+    int result = NumDes(number);
     Console.WriteLine("Вторая цифра в этом числе = " + result);
 }
 else Console.WriteLine("Ваше число не является трехзначным");
 */
 
 
-
 // Задача 2 - Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 /*
-Console.Write("Введите любое положительное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int result;
-bool control = true;
 
 bool NumControl(int num)
 {
-    if (num / 100 == 0) control = false;
-    return control;
+    if (num / 100 == 0) return false;
+    return true;
 }
 
 int FindThirdNum(int fullNum)
@@ -39,33 +33,29 @@ int FindThirdNum(int fullNum)
     {
         numTemp = numTemp / 10;
     }
-    return result = numTemp % 10;
+    return numTemp % 10;
 }
 
-control = NumControl(number);
+Console.Write("Введите любое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool control = NumControl(number);
 if (control == true)
 {
-    result = FindThirdNum(number);
+    int result = FindThirdNum(number);
     Console.WriteLine("Третья цифра в числе = " + result);
 }
 else Console.WriteLine("В этом числе нет 3-й цифры");
-
 */
 
 
 // Задача 3 - Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 /*
-Console.Write("Какой сегодня день недели? Укажите номер в диапазоне от 1 до 7: ");
-int numDayOfWeek = Convert.ToInt32(Console.ReadLine());
-bool result = false;
 
 bool IsWeekend(int num)
 {
-    if (num == 6 || num == 7)
-    {
-        result = true;
-    }
-    return result;
+    if (num == 6 || num == 7) return true;
+    return false;
 }
 
 void PrintAnswer(bool res)
@@ -74,14 +64,19 @@ void PrintAnswer(bool res)
     else Console.WriteLine("Этот день не является выходным");
 }
 
+Console.Write("Какой сегодня день недели? Укажите номер в диапазоне от 1 до 7: ");
+int numDayOfWeek = Convert.ToInt32(Console.ReadLine());
+
+
 if (numDayOfWeek < 1 || numDayOfWeek > 7)
 {
     Console.WriteLine("Вы указали день недели некорректно. В неделе 7 дней. Нумерация с единицы, которая соответствует понедельнику.");
 }
 else
 {
-    IsWeekend(numDayOfWeek);
+    bool result = IsWeekend(numDayOfWeek);
     PrintAnswer(result);
 }
 */
+
 
